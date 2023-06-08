51e1ae079ce271ad89ab3d2425cc3d319fb414b5
Naoya-Otani <naoyaotani914@gmail.com>
2023-06-08 16:53:52 +0900

    feature / loading UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import "@/styles/globals.css";
 import { Gothic_A1, Noto_Sans_JP } from "next/font/google";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import Router from "next/router";
+import { useEffect, useState } from "react";
+import Loading from "@/components/parts/common/Loading";
 
+// fonts setting
 const gothicA1 = Gothic_A1({
   weight: ["100", "200", "300", "400", "500"],
   subsets: ["latin"],
   display: "swap",
 });
-
 const notoSansJP = Noto_Sans_JP({
   weight: ["100", "300", "400", "500", "700", "900"],
   subsets: ["latin"],
@@ -16,10 +19,30 @@ const notoSansJP = Noto_Sans_JP({
 });
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+
+    const end = () => {
+      setLoading(false);
+    };
+
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
   return (
     <>
       <SessionProvider>
-        <Component {...pageProps} />
+        {loading ? <Loading /> : <Component {...pageProps} />}
       </SessionProvider>
       <style jsx global>
         {`
