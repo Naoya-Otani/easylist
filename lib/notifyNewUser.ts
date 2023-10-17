e6509070f756ebe6d103608eecb92246686b403f
Naoya-Otani <naoyaotani914@gmail.com>
2023-10-18 00:25:35 +0900

    notify discord

---
+++ lib/notifyNewUser.ts
@@ -0,0 +1,30 @@
+const fetch = require("node-fetch");
+
+async function notifyNewUser(user: any) {
+  try {
+    const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
+    const payload = {
+      content: `新しいユーザー登録: ${user.name}`,
+    };
+    const response = await fetch(webhookUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+    if (response.status === 204) {
+      console.log("Discord通知を送信しました: ", payload.content);
+    } else {
+      console.error(
+        "Discord通知の送信に失敗しました:",
+        response.status,
+        await response.text()
+      );
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default notifyNewUser;
