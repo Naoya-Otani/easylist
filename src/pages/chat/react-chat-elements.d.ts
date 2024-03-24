d3e0961c843e07e536a6782119d89ba8a631550c
Ryuya kawaguchi <106297880+lll-kkk-ryuya@users.noreply.github.com>
2024-03-24 21:35:16 +0900

    Revert "Feature/chatroom"

--- src/pages/chat/react-chat-elements.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'react-chat-elements' {
-    export class MessageBox extends React.Component<any, any> {}
-    export class Input extends React.Component<any, any> {}
-    export class MessageList extends React.Component<any, any> {}
-    // 必要に応じて他のコンポーネントも追加
-  }
