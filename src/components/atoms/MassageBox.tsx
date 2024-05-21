a876cdb8b946196bbd3c0441cf0f0b5e441f56db
Naoya-Otani <naoyaotani914@gmail.com>
2024-05-21 19:46:12 +0900

    modified UI

--- src/components/atoms/MassageBox.tsx
+++ src/components/atoms/MassageBox.tsx
@@ -1,32 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import Logo from '@/src/components/atoms/icon/Logo';
-import UserIcon from '@/src/components/atoms/icon/Chatuser';
-import TextStreamer from '@/src/components/parts/chat/TextStreamer';
-import useWebSocket from '@/src/hooks/useWebSocket';
+import React, { useState, useEffect } from "react";
+import Logo from "@/src/components/atoms/icon/Logo";
+import UserIcon from "@/src/components/atoms/icon/Chatuser";
+import TextStreamer from "@/src/components/parts/chat/TextStreamer";
+import useWebSocket from "@/src/hooks/useWebSocket";
 
 interface MessageBoxProps {
-  id: number,
-  position: 'left' | 'right';
+  id: number;
+  position: "left" | "right";
   text: string;
-  source: 'user' | 'bot';
+  source: "user" | "bot";
   loop?: boolean;
-  isStreaming:boolean;
+  isStreaming: boolean;
 }
 
-
-const MessageBox: React.FC<MessageBoxProps> = ({ id, position, text, source, loop = false, isStreaming}) => {
+const MessageBox: React.FC<MessageBoxProps> = ({
+  id,
+  position,
+  text,
+  source,
+  loop = false,
+  isStreaming,
+}) => {
   const [message, setMessage] = useState<string | null>(null);
-  const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL ? process.env.NEXT_PUBLIC_WEBSOCKET_URL : "";
+  const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL
+    ? process.env.NEXT_PUBLIC_WEBSOCKET_URL
+    : "";
   const { messages } = useWebSocket(WEBSOCKET_URL);
   const [loading, setLoading] = useState(true);
   const [donotcreate, setDonotCreate] = useState(false);
 
   useEffect(() => {
-    let bufferedMessage = ''; 
-  
+    let bufferedMessage = "";
+
     for (const msg of messages) {
-      if (msg.id === id && msg.source === 'bot') {
-        
+      if (msg.id === id && msg.source === "bot") {
         const newContent = msg.content;
         bufferedMessage += newContent;
       }
@@ -34,7 +41,7 @@ const MessageBox: React.FC<MessageBoxProps> = ({ id, position, text, source, loo
 
     if (bufferedMessage.length > 0) {
       console.log("Buffered message:", bufferedMessage);
-      setMessage(bufferedMessage); 
+      setMessage(bufferedMessage);
       setLoading(false);
     } else {
       console.log("No matching message found for ID:", id);
@@ -45,25 +52,40 @@ const MessageBox: React.FC<MessageBoxProps> = ({ id, position, text, source, loo
     }
   }, [messages, id, isStreaming]);
 
-  
-
   return (
-    <div className={`flex flex-col ${position === 'left' ? 'items-start' : 'items-end'} mb-2`}>
+    <div
+      className={`flex flex-col ${
+        position === "left" ? "items-start" : "items-end"
+      } mb-2`}
+    >
       <div className="flex items-center">
-        {source === 'user' ? <UserIcon /> : <Logo className="w-6 h-6" />}
-        <span className="ml-2 font-semibold">{source === 'user' ? 'Student' : 'EASYLIST-BOT'}</span>
+        {source === "user" ? <UserIcon /> : <Logo className="w-6 h-6" />}
+        <span className="ml-2 font-semibold">
+          {source === "user" ? "あなた" : "楽単くん"}
+        </span>
       </div>
-      <div className={`mt-1 p-3 bg-gray-50 rounded-lg ${source === 'user' ? 'rounded-tr-none' : 'rounded-tl-none'}`}>
+      <div
+        className={`mt-1 p-3 bg-gray-50 rounded-lg ${
+          source === "user" ? "rounded-tr-none" : "rounded-tl-none"
+        }`}
+      >
         <TextStreamer text={text} loop={loop} />
       </div>
       <div className="flex flex-col mt-6">
         <div className="flex items-center">
           <Logo className="w-6 h-6" />
-          <span className="ml-2 font-semibold">EASYLIST-BOT</span>
+          <span className="ml-2 font-semibold">楽単くん</span>
         </div>
-        <div className={`p-3 bg-gray-50 rounded-lg ${position === 'left' ? 'rounded-tr-none' : 'rounded-tl-none'}`}>
+        <div
+          className={`p-3 bg-gray-50 rounded-lg ${
+            position === "left" ? "rounded-tr-none" : "rounded-tl-none"
+          }`}
+        >
           {loading ? (
-            <TextStreamer text="Loading using vector search......." loop={true} />
+            <TextStreamer
+              text="Loading using vector search......."
+              loop={true}
+            />
           ) : (
             <p className="text-sm">{message}</p>
           )}
--- src/components/parts/chat/MessageInput.tsx
+++ src/components/parts/chat/MessageInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import SendBtn from "../../atoms/Button/SendBtn";
 
 interface MessageInputProps {
@@ -14,48 +14,64 @@ const MessageInput: React.FC<MessageInputProps> = ({
   onSendMessage,
   isStreaming,
 }) => {
-    const [isComposing, setIsComposing] = useState(false);
+  const [isComposing, setIsComposing] = useState(false);
 
-    const handleFormSubmit = (event: React.FormEvent) => {
+  const handleFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!inputValue.trim() || isComposing || isStreaming) return;
+    onSendMessage(inputValue);
+    setInputValue("");
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (
+      event.key === "Enter" &&
+      !event.shiftKey &&
+      !isComposing &&
+      !isStreaming
+    ) {
       event.preventDefault();
-      if (!inputValue.trim() || isComposing || isStreaming) return; 
-      onSendMessage(inputValue);
-      setInputValue('');
-    };
+      handleFormSubmit(event as unknown as React.FormEvent);
+    }
+  };
 
-   
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-      if (event.key === 'Enter' && !event.shiftKey && !isComposing && !isStreaming) {
-        event.preventDefault();
-        handleFormSubmit(event as unknown as React.FormEvent);
-      }
-    };
+  const handleCompositionStart = () => {
+    setIsComposing(true);
+  };
 
-    const handleCompositionStart = () => {
-      setIsComposing(true);
-    };
+  const handleCompositionEnd = () => {
+    setIsComposing(false);
+  };
 
-    const handleCompositionEnd = () => {
-      setIsComposing(false);
-    };
-
-    return (
-      <form onSubmit={handleFormSubmit} className="bg-white rounded-lg shadow flex flex-col space-y-2 w-5/6 sm:w-3/5 mx-auto">
-        <div className="flex items-center border border-gray-400 rounded-md transition-colors relative">
-          <input
-            type="text"
-            className="flex-1 rounded-md focus:outline-none border-transparent"
-            placeholder={isStreaming ? "生成している途中での会話はできません" : "Message EASYLIST-BOT..."}
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            onKeyDown={handleKeyDown} 
-            onCompositionStart={handleCompositionStart}
-            onCompositionEnd={handleCompositionEnd}
-          />
-          <SendBtn onClick={handleFormSubmit as unknown as React.MouseEventHandler<HTMLButtonElement>} disabled={isStreaming} />
-        </div>
-      </form>
-    );
+  return (
+    <form
+      onSubmit={handleFormSubmit}
+      className="bg-white rounded-lg shadow flex flex-col space-y-2 w-5/6 sm:w-3/5 mx-auto"
+    >
+      <div className="flex items-center border border-gray-400 rounded-md transition-colors relative">
+        <input
+          type="text"
+          className="flex-1 rounded-md focus:outline-none focus:ring-0 border-0"
+          placeholder={
+            isStreaming
+              ? "生成している途中での会話はできません"
+              : "楽単くんにメッセージ"
+          }
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+          onCompositionStart={handleCompositionStart}
+          onCompositionEnd={handleCompositionEnd}
+        />
+        <SendBtn
+          onClick={
+            handleFormSubmit as unknown as React.MouseEventHandler<HTMLButtonElement>
+          }
+          disabled={isStreaming}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default MessageInput;
