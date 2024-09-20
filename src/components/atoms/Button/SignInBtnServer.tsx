7db9bf472f618f830b671722e0a78e75bef355e2
Naoya-Otani <naoyaotani914@gmail.com>
2024-09-20 12:06:04 +0900

    fix: set destination after signin

--- src/components/atoms/Button/SignInBtnServer.tsx
+++ src/components/atoms/Button/SignInBtnServer.tsx
@@ -6,7 +6,9 @@ const SignInBtnServer = async () => {
 		<form
 			action={async () => {
 				"use server";
-				await signIn("google");
+				await signIn("google", {
+					redirectTo: process.env.NEXT_PUBLIC_BASE_URL,
+				});
 			}}
 		>
 			<button
